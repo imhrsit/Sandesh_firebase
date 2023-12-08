8ef525825cb59e9cea62809399d28f5659246cdc
imhrsit <tiwariharsh1408@gmail.com>
2023-12-08 12:45:30 +0530

    Backend and push notifications

--- chat_app/lib/screens/chat.dart
+++ chat_app/lib/screens/chat.dart
@@ -1,8 +1,34 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_messages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget{
+class ChatScreen extends StatefulWidget{
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async{
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();    //notification settings can also be added here!
+
+    fcm.subscribeToTopic('chat');     // Backend on node, not done 
+
+    final token = await fcm.getToken();
+    print(token);   //you could send this token via HTTP or firestore SDK instead of copy pasting it.
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +47,10 @@ class ChatScreen extends StatelessWidget{
             )
         ],
       ),
-      body: Center(
-        child: Text('Logged in!'),
-      ),
+      body: Column(children: [
+        Expanded(child: ChatMessages()),
+        NewMessage(),
+      ],),
     );
   }
 }
